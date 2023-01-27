015015558ac7dfdf3aab235668c44c482b5f3e22
Mohammad Aslam <Taiyab.aslam728@gmail.com>
2023-01-26 20:10:18 -0600

    couldnt get my render posts function to work will continue tommorow

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 /* Imports */
+import { signIn, signUp, getPost, getPosts } from './fetch-utils.js';
 
 /* Get DOM Elements */
+const loginButtonEl = document.querySelector('.login-button');
+const createButtonEl = document.querySelector('.create-button');
 
 /* State */
-
+let postsData = [];
 /* Events */
+window.addEventListener('load', () => {
+    const posts = await getPosts();
+    postsData = posts;
+    // call my display function here when its done
+});
 
 /* Display Functions */
 
--- fetch-utils.js
+++ fetch-utils.js
@@ -3,3 +3,24 @@ const SUPABASE_KEY =
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InltdmZpbGNpdnV0eG9qbWpmaW9zIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzQ1MTg5OTEsImV4cCI6MTk5MDA5NDk5MX0.Rf8GvkCe-vF7Fxy6rR0pVu_QXX67mnTu95dC8XW5sdY';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+export async function signIn(email, password) {
+    const { data, error } = await client.auth.signIn({
+        email: email,
+        password: password
+    });
+}
+
+export async function signUp(email, password) {
+    const { data, error } = await client.auth.signUp({
+        email: email,
+        password: password
+    });
+}
+
+export async function getPosts() {
+    const { data, error } = await client
+        .from('board')
+        .select('*');
+    return data;
+}
